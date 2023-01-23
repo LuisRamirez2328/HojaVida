94f2d4fd96af5a03981d49911e6a63edea8dba29
LuisAntonio2823 <106568146+LuisAntonio2823@users.noreply.github.com>
2023-01-22 21:45:29 -0600

    "deploy"

--- HojaDeVida/deploy.sh
+++ HojaDeVida/deploy.sh
@@ -18,10 +18,10 @@ git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com <USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO> 
-#  git push -f git@github.com:LuisRamirez2328/HojaVida.git main:gh-pages
+# git push -f git@github.com:LuisRamirez2328/HojaVida.git main:gh-pages
 
 cd-
